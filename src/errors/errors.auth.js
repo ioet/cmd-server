2323aa905f79d91bd4291c71d2e4140c107f1afd
boscoand <boscoand@hotmail.com>
2018-04-09 11:05:05 -0500

    Load version app from .env file

--- src/errors/errors.auth.js
+++ src/errors/errors.auth.js
@@ -1,26 +1,27 @@
 module.exports = {
     errorDataBaseConnection: (err, res) => {
         console.log(err)
-        res.status(400).json({errors: { global: "Error from database connection." } })
+        res.status(400).json({errors: { global: "- Error from database connection." } })
     },
     errorIncorrectEmail: (res) => {
-        console.log("Error: Incorrect email")
+        console.log("- Error: Incorrect email")
         res.status(400).json({errors: { global: "Incorrect email." } })
     },
     errorNotFoundRestaurants: (res) => {
-        console.log("Error: Not found restaurants")
+        console.log("- Error: Not found restaurants")
         res.status(400).json({errors: { global: "Error during retrieving the list of restaurants" } })
     },
     errorIncorrectPassword: (res) => {
-        console.log("Error: Incorrect password")
+        console.log("- Error: Incorrect password")
         res.status(400).json({errors: { global: "Incorrect password." } })
     },
-    errorEmailIsRegistered: (res) => {
-        console.log("Error: The email is registered")
+    errorEmailIsRegistered: (
+      res) => {
+        console.log("- Error: The email is registered")
         res.status(400).json({errors: { global: "The email is already registered." } })
     },
     errorEncriptingPassword: (err, res) => {
         console.log(err)
-        res.status(400).json({errors: { global: "Error encrypting password." } })
+        res.status(400).json({errors: { global: "- Error encrypting password." } })
     }
 }
